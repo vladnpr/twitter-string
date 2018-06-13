499dbde9964cf73c822e02156ac2e4110b22b895
Vladislav <vladnpr@gmail.com>
2018-06-13 08:59:02 +0300

    symfony class-loader

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once 'src/Messages.php';
+define('BASE_DIR', __DIR__);
+use Src;
+require_once BASE_DIR . '/vendor/autoload.php';
 require_once 'config.php';
+require_once 'vendor/symfony/class-loader/Psr4ClassLoader.php';
 
-$test = new \Src\Messages($settings);
+$loader = new \Symfony\Component\ClassLoader\Psr4ClassLoader();
+$loader->addPrefix('Src\Messages', __DIR__.'/src/');
+
+$loader->register();
+$loader->loadClass(Src\Messages::class);
+echo '<pre>';
+var_dump($loader);
+
+$test = new Src\Messages($settings);
+
+
+
