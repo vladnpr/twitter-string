0c26458ea7fea42c7345f5c28244edadc47342de
vladnpr <vladnpr@gmail.com>
2018-06-13 11:08:00 +0300

    Revert "symfony class-loader"
    This reverts commit 499dbde9964cf73c822e02156ac2e4110b22b895.

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-define('BASE_DIR', __DIR__);
-use Src;
-require_once BASE_DIR . '/vendor/autoload.php';
+require_once 'src/Messages.php';
 require_once 'config.php';
-require_once 'vendor/symfony/class-loader/Psr4ClassLoader.php';
 
-$loader = new \Symfony\Component\ClassLoader\Psr4ClassLoader();
-$loader->addPrefix('Src\Messages', __DIR__.'/src/');
-
-$loader->register();
-$loader->loadClass(Src\Messages::class);
-echo '<pre>';
-var_dump($loader);
-
-$test = new Src\Messages($settings);
-
-
-
+$test = new \Src\Messages($settings);
--- src/Messages.php
+++ src/Messages.php
@@ -1,28 +1,12 @@
 <?php
 namespace Src;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
-
 class Messages
 {
     private $consumer_key, $consumer_secret, $oauth_token, $oauth_secret;
 
     public function __construct(array $config)
     {
-        $this->consumer_key = $config['consumer_key'];
-        $this->consumer_secret = $config['consumer_secret'];
-        $this->oauth_token = $config['oauth_token'];
-        $this->oauth_secret = $config['oauth_secret'];
-    }
-
-    public function getConnection()
-    {
-        $connection = new TwitterOAuth(
-            $this->consumer_key,
-            $this->consumer_secret,
-            $this->oauth_token,
-            $this->oauth_secret
-            );
-        var_dump($connection);
+        var_dump($config);
     }
 }
